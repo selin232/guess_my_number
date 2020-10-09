f8106247c562bd1b2f79cbd57004d3e3f77b6512
Selin Kandiyoti <selin.kandiyotieskenazi@epfl.ch>
2020-10-09 14:30:28 +0200

    Amélioration 1 de computer_player.py avec ajustement de min et max

--- computer_player.py
+++ computer_player.py
@@ -15,11 +15,18 @@ import random
 from guess_my_number import MIN, MAX, GuessMachine
 
 if __name__ == '__main__':
+    min = MIN
+    max = MAX
+    
     guess_machine = GuessMachine()
     while True:
-        attempt = random.randint(MIN,MAX)
+        attempt = random.randint(min,max)
         result = guess_machine.guess(attempt)
         print('tried %d : %s' % (attempt, result))
         if result == 'found':
             print('Finished in %d attempts.' % guess_machine.number_of_attempt)
             break
+        elif result=='too low...':
+            min = attempt + 1
+        else:
+            max = attempt - 1
